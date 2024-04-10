421ad8a9d8b3b7cf9c0cd85e55d625aca67211af
lack <598223084@qq.com>
2024-04-10 09:17:41 +0800

    change license to LGPL

--- flow_node/gateway/parallel/doc.go
+++ flow_node/gateway/parallel/doc.go
@@ -2,22 +2,18 @@
 /*
    Copyright 2023 The bpmn Authors
 
-   This program is offered under a commercial and under the AGPL license.
-   For AGPL licensing, see below.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-   AGPL licensing:
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
+   This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Affero General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Affero General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library;
 */
 
 package parallel
